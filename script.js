680938f327c986fb52e701296048dfedfbecca7c
Javier Aranda Pizarro <jiaranda@uc.cl>
2020-10-21 20:18:30 -0300

    getCards function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+const getCards = async ({ data, baseElement }) => {
+  const SKIP = 70;
+  const t0 = Date.now();
+  for (let i = 0; i < data.length - 1; i++) {
+    await createCard({ data: data[i], baseElement, id: i });
+  }
+  const t1 = Date.now();
+  console.log(t1 - t0);
+};
+
 const main = async () => {
   const data = await loadData("data/fifa_20_data.csv");
-  const body = d3.select(".cards-container");
-  const SKIP = 70;
-
-  for (let i = 0; i < data.length - 1; i++) {
-    await createCard({ data: data[i], baseElement: body, id: i });
-    i += SKIP;
-  }
+  const baseElement = d3.select(".cards-container");
+  await getCards({ data, baseElement });
 };
 
 main().catch((err) => console.error(err));
