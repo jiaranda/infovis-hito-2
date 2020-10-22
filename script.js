8659cb92a9a8aea97cc302e239c2cee0356b63c8
Javier Aranda Pizarro <jiaranda@uc.cl>
2020-10-22 15:12:53 -0300

    set skip to 0

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const getCards = async ({ data, baseElement }) => {
-  const SKIP = 70;
+  const SKIP = 0;
   const t0 = Date.now();
   for (let i = 0; i < data.length - 1; i++) {
-    await createCard({ data: data[i], baseElement, id: i });
+    createCard({ data: data[i], baseElement, id: i });
     i += SKIP;
   }
   const t1 = Date.now();
@@ -72,8 +72,8 @@ const getSummary = async ({ data }) => {
 const main = async () => {
   const data = await loadData("data/fifa_20_data.csv");
   const baseElement = d3.select("#cards-container");
-  await getCards({ data, baseElement });
   await getSummary({ data });
+  await getCards({ data, baseElement });
 };
 
 main().catch((err) => console.error(err));
